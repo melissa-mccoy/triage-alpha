3f62766ddd56cb32d553f41e78bb9c0b3c9dae5a
melissa-mccoy <melissa.l.mccoy@gmail.com>
2015-08-02 23:22:26 +0100

    completed process of Loop through Data table,move same-case rows into separate tables, join the tables

--- prepareData.m
+++ prepareData.m
@@ -1,31 +1,44 @@
-%% Loop through Data table and combine question & answer columns from the same cases
-caseLog = [];
-caseTableArray = {};
-CaseTable = table;
-CaseData.case_no = {};
+%% Loop through Data table,move same-case rows into separate tables, join the tables
+CaseTable1 = table;
+CaseTable1.case_no = {};
+CaseTable2 = table;
+CaseTable2.case_no = {};
+CaseTable3 = table;
+CaseTable3.case_no = {};
+CaseTable4 = table;
+CaseTable4.case_no = {};
+CaseTable5 = table;
+CaseTable5.case_no = {};
+CaseTable6 = table;
+CaseTable6.case_no = {};
+CaseTable7 = table;
+CaseTable7.case_no = {};
+CaseTable8 = table;
+CaseTable8.case_no = {};
+CaseTable9 = table;
+CaseTable9.case_no = {};
+CaseTable10 = table;
+CaseTable10.case_no = {};
+caseTableArray = {CaseTabe1,CaseTabe2,CaseTabe3,CaseTabe4,CaseTabe5,CaseTabe6,CaseTabe7,CaseTabe8,CaseTabe9,CaseTabe10};
 
 for row = 1:size(DataTemp,1)
     caseNo = DataTemp.case_no(row);
     for n = 1:size(caseTableArray)
-        caseTable = caseTableArray(n);
-        if any(ismember(caseTable.case_no, caseNo))
-            if n<>size(caseTablesArray)
-                continue
-            else
-                eval([strcat('CaseTable',int2str(n+1)) '=table;'])
-                eval([strcat('CaseTable',int2str(n+1)) '.case_no = {};'])
-                eval(['caseTableArray(end+1) = {' strcat('CaseTable',int2str(n+1)) '};'])
-            end
-        elseif 
+        currentTable = caseTableArray(n);
+        if any(ismember(currentTable.case_no, caseNo))
+            continue
+        elseif n == 1
+            currentTable = DataTemp(row,2:end);
+            break
+        else
+            currentTable = DataTemp(row,19:end);
+            break     
+        end
     end
-        
-        
-    DataTemp(2:end)
-    if any(ismember(CaseData.case_no, caseNo))
-        
-    if strfind(char(OdysseyData8.Properties.VariableNames(col)),'questxt') == 1
-        
-    end
-    end
+end
 
-    
+for t = 2:size(caseTableArray,2)
+    CaseTable1 = join(CaseTable1,caseTableArray(t),'Keys',1);
+end
+
+CaseTable = CaseTable1;
