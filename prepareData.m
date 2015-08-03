f82b793bb48c2ac2efeec35f03fe88ffe5c36c3c
melissa-mccoy <melissa.l.mccoy@gmail.com>
2015-08-03 01:28:18 +0100

    fixed bugs in process of Loop through Data table,move same-case rows into separate tables, join the tables

--- prepareData.m
+++ prepareData.m
@@ -1,44 +1,59 @@
 %% Loop through Data table,move same-case rows into separate tables, join the tables
-CaseTable1 = table;
-CaseTable1.case_no = {};
-CaseTable2 = table;
-CaseTable2.case_no = {};
-CaseTable3 = table;
-CaseTable3.case_no = {};
-CaseTable4 = table;
-CaseTable4.case_no = {};
-CaseTable5 = table;
-CaseTable5.case_no = {};
-CaseTable6 = table;
-CaseTable6.case_no = {};
-CaseTable7 = table;
-CaseTable7.case_no = {};
-CaseTable8 = table;
-CaseTable8.case_no = {};
-CaseTable9 = table;
-CaseTable9.case_no = {};
-CaseTable10 = table;
-CaseTable10.case_no = {};
-caseTableArray = {CaseTabe1,CaseTabe2,CaseTabe3,CaseTabe4,CaseTabe5,CaseTabe6,CaseTabe7,CaseTabe8,CaseTabe9,CaseTabe10};
+CaseTable1 = cell2table(cell(0,size(DataTemp,2)));
+CaseTable1.Properties.VariableNames = DataTemp.Properties.VariableNames;
+CaseTable2 = cell2table(cell(0,61));
+CaseTable2.Properties.VariableNames = DataTemp.Properties.VariableNames([1,19:end]);
+CaseTable3 = cell2table(cell(0,61));
+CaseTable3.Properties.VariableNames = DataTemp.Properties.VariableNames([1,19:end]);
+CaseTable4 = cell2table(cell(0,61));
+CaseTable4.Properties.VariableNames = DataTemp.Properties.VariableNames([1,19:end]);
+CaseTable5 = cell2table(cell(0,61));
+CaseTable5.Properties.VariableNames = DataTemp.Properties.VariableNames([1,19:end]);
+CaseTable6 = cell2table(cell(0,61));
+CaseTable6.Properties.VariableNames = DataTemp.Properties.VariableNames([1,19:end]);
+CaseTable7 = cell2table(cell(0,61));
+CaseTable7.Properties.VariableNames = DataTemp.Properties.VariableNames([1,19:end]);
+CaseTable8 = cell2table(cell(0,61));
+CaseTable8.Properties.VariableNames = DataTemp.Properties.VariableNames([1,19:end]);
+CaseTable9 = cell2table(cell(0,61));
+CaseTable9.Properties.VariableNames = DataTemp.Properties.VariableNames([1,19:end]);
+CaseTable10 = cell2table(cell(0,61));
+CaseTable10.Properties.VariableNames = DataTemp.Properties.VariableNames([1,19:end]);
+caseTableArray = {CaseTable1,CaseTable2,CaseTable3,CaseTable4,CaseTable5,CaseTable6,CaseTable7,CaseTable8,CaseTable9,CaseTable10};
+clearvars CaseTable1 CaseTable2 CaseTable3 CaseTable4 CaseTable5 CaseTable6 CaseTable7 CaseTable8 CaseTable9 CaseTable10;
 
 for row = 1:size(DataTemp,1)
-    caseNo = DataTemp.case_no(row);
-    for n = 1:size(caseTableArray)
-        currentTable = caseTableArray(n);
+    caseNo = DataTemp.case_no{row};
+    for n = 1:size(caseTableArray,2)
+        currentTable = caseTableArray{n};
         if any(ismember(currentTable.case_no, caseNo))
             continue
         elseif n == 1
-            currentTable = DataTemp(row,2:end);
+            caseTableArray{n} = [caseTableArray{n}; DataTemp(row,:)];
             break
         else
-            currentTable = DataTemp(row,19:end);
+            caseTableArray{n} = [caseTableArray{n}; DataTemp(row,[1,19:end])];
             break     
         end
     end
 end
 
 for t = 2:size(caseTableArray,2)
-    CaseTable1 = join(CaseTable1,caseTableArray(t),'Keys',1);
+    if size(caseTableArray{t},1)>0
+        caseTableArray{1} = outerjoin(caseTableArray{1},caseTableArray{t},'Keys',1);
+    end
 end
 
-CaseTable = CaseTable1;
+CaseTable = caseTableArray{1};
+
+%% Loop through cols & rows of CaseTable Table, add questions as cols of Feature Table and ans as values
+FeaturesTable = table;
+for c = 1:size(CaseTable,2)
+    if findstr('questxt',CaseTable.Properties.VariableNames{c})
+        for r = 1:size(CaseTable,1)
+            
+        end
+    elseif findstr('ans',CaseTable.Properties.VariableNames{c})
+    else
+    end
+end
