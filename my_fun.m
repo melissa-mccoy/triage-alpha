31d053f88b1a4927d3c38e938323995952f366c5
melissa-mccoy <melissa.l.mccoy@gmail.com>
2015-08-22 16:33:33 +0100

    removed several now made unnneeded files from packages in main folder

--- my_fun.m
+++ my_fun.m
@@ -1,11 +1,12 @@
 function [ criterion ] = my_fun(trainX,trainY,testX,testY)
 
-%     SVMModel = fitcsvm(trainXMat,trainY,'Standardize',true,'KernelFunction','linear','ClassNames',{'SELF','DR'});
-%     [labelTest,scoreTest] = predict(SVMModel,testX);
-%     CP_Test = classperf(testY, labelTest,'Positive',{'DR'}, 'Negative', {'SELF'});
+    SVMModel = fitcsvm(trainX,trainY,'Standardize',true,'KernelFunction','linear');
+    [labelTest,scoreTest] = predict(SVMModel,testX);
+    CP_Test = classperf(testY, labelTest,'Positive',1, 'Negative', 0);
+    criterion = CP_Test.ErrorRate;
 
-SVMModelCV = fitcsvm(trainX,trainY,'Crossval','on','KFold',10,'Standardize',true,'KernelFunction','linear','ClassNames',{'SELF','DR'});
-criterion = kfoldLoss(SVMModelCV);
+% SVMModelCV = fitcsvm(trainX,trainY,'Crossval','on','KFold',10,'Standardize',true,'KernelFunction','linear','ClassNames',{'SELF','DR'});
+% criterion = kfoldLoss(SVMModelCV);
  
 end
 
